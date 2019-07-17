e5a85e1de74a73541efa85627f813e8bcf859ac3
CalvinYusnoveri <calvin.yusnoveri023290031@gmail.com>
2019-07-17 22:09:48 +0800

    added body-parser

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const app = express()
+const bodyParser = require('body-parser')
+
+// Tell the bodyparser middleware to accept certain amount of data only
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 
 app.post('/api/QWERTY-SIA', (req, res, next) => {
   console.log('/api/QWERTY-SIA is called...')
