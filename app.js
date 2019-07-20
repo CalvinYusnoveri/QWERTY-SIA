16144d10b7f700f8750f9631355b4c64fb4878b4
LT2333 <0817rainy@gmail.com>
2019-07-20 19:49:58 +0800

    merge

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app=express();
-=======
 // import npm dependencies
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const inventory_routes = require('./server/routes/api/InventoryRoutes');
 
 // Tell the bodyparser middleware to accept certain amount of data only
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.use(cors());
 
 app.post('/api/QWERTY-SIA', async (req, res, next) => {
   console.log('/api/QWERTY-SIA is called...')
@@ -24,17 +21,11 @@ app.post('/api/QWERTY-SIA', async (req, res, next) => {
   let action = req.body.queryResult.action
   console.log('action: ', action)
 
-
   next()},
   (req, res) => res.send(req.body))
->>>>>>> 1e92fe7860a126bee6af66c94aeb1aca94d07aab
 
-//Middleware
-app.use(bodyParser.json());
-app.use(cors());
 
-const inventory_routes = require('./server/routes/api/InventoryRoutes');
-//Pass to te router
+
 app.use('/api/inventory',inventory_routes);
 
 const port = process.env.PORT || 5000;
