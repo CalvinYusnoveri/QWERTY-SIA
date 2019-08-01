af8909cabd075824f9ce0a9e3bd3dc1b3b4791c2
CalvinYusnoveri <calvin.yusnoveri023290031@gmail.com>
2019-08-01 22:38:44 +0800

    replace works on all bubbles

--- server/routes/api/replace-url.js
+++ server/routes/api/replace-url.js
@@ -6,30 +6,21 @@ async function replaceURL(req, res, next) {
     console.log('action === replace-url')
 
     // get the number of inline_keyboard objects
-    console.log(req.body.queryResult.fulfillmentMessages[0].payload.telegram.reply_markup.inline_keyboard[0])
-    let ikb = await req.body.queryResult.fulfillmentMessages[0].payload.telegram.reply_markup.inline_keyboard[0]
     let params = await req.body.queryResult.parameters
-    let iter = await ikb.length
-    console.log('there are this many inline_keyboard objects:', iter)
 
     // for inline_keyboard objects, replace @blah@ with param values (val) in url and text props
-    for (var i=0; i < iter; i++) {
-      try { for (var key in params) {
-        let val = await params[key]
-        let rgx = new RegExp(`@${key}@`,"g")
+    try { for (var key in params) {
+      let val = await params[key]
+      let rgx = new RegExp(`@${key}@`,"g")
 
-        console.log(`now in inline_keyboard obj ${i}; replacing, ${rgx} with ${val}`)
-        ikb[i].text = await ikb[i].text.replace(rgx, val)
-        ikb[i].url = await ikb[i].url.replace(rgx, val) }
+      console.log(`now replacing, ${rgx} with ${val}`)
+      req.body.queryResult.fulfillmentMessages = await JSON.parse(JSON.stringify(req.body.queryResult.fulfillmentMessages).replace(rgx, val))
+      console.log('result:', JSON.stringify(req.body.queryResult.fulfillmentMessages)) }
 
-      } catch (err) {console.log(err); continue}
-    }
-
+    } catch (err) {console.log(err)}
   next()
-  }
 
-  else { console.log('action !== replace-url'); next() }
-
+  } else { console.log('action !== replace-url'); next() }
 }
 
 async function pdf2JSON(req, res, next) {
